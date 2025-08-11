55ac46b5fb95d6e887823c6a16ae57b0aaaa00b3
SMB <123291842+SMB-5@users.noreply.github.com>
2025-08-11 12:05:52 +0800

    finished rpg select functionality

--- Assets/Scripts/RPGHandler.cs
+++ Assets/Scripts/RPGHandler.cs
@@ -8,9 +8,12 @@ public class RPGHandler : MonoBehaviour
     public TextMeshProUGUI BattleText;
     public TextMeshProUGUI HPText;
     public TextMeshProUGUI GoblinText;
+    public GameObject S_Goblin_256x256;
     public static string[] Situation = {"Fight", "Heal", "Run"};
     private string chosenSituation;
     private bool Pressed;
+    public float decisionTime;
+    private bool OutofTime;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +21,9 @@ public class RPGHandler : MonoBehaviour
         int randomIndex = Random.Range(0, Situation.Length);
         chosenSituation = Situation[randomIndex];
         Debug.Log("Situation: " + chosenSituation);
+        decisionTime = 3.0f;
         Pressed = false;
+        OutofTime = false;
         BattleText.text = "A goblin blocks your path. What do you do?\nLeft Arrow Key - Fight                         Right Arrow Key - Heal\n                             Down Arrow Key - Run";
 
         switch (chosenSituation)
@@ -44,43 +49,65 @@ public class RPGHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.LeftArrow) && !Pressed)
+        decisionTime -= Time.deltaTime;
+
+        if (decisionTime <= 0.0f && !Pressed && !OutofTime)
+        {
+            Debug.Log("TIME UP");
+            OutofTime = true;
+            StartCoroutine(TimeUp());
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow) && !Pressed && decisionTime >= 0.0f)
         {
             if(chosenSituation == "Fight")
             {
                 Debug.Log("CORRECT");
                 Pressed = true;
+                BattleText.text = "You Attack the Goblin.";
+                StartCoroutine(FightCorrect());
             }
             else
             {
                 Debug.Log("WRONG");
                 Pressed = true;
+                BattleText.text = "The Goblin Attacks!";
+                StartCoroutine(FightWrong());
             }
         }
-        if(Input.GetKeyDown(KeyCode.RightArrow) && !Pressed)
+        if(Input.GetKeyDown(KeyCode.RightArrow) && !Pressed && decisionTime >= 0.0f)
         {
             if(chosenSituation == "Heal")
             {
                 Debug.Log("CORRECT");
                 Pressed = true;
+                BattleText.text = "You healed all your HP.";
+                HPText.text = "HP: 20";
+                HPText.color = Color.white;
+                StartCoroutine(HealCorrect());
             }
             else
             {
                 Debug.Log("WRONG");
                 Pressed = true;
+                BattleText.text = "The Goblin Attacks!";
+                StartCoroutine(HealWrong());
             }
         }
-        if(Input.GetKeyDown(KeyCode.DownArrow) && !Pressed)
+        if(Input.GetKeyDown(KeyCode.DownArrow) && !Pressed && decisionTime >= 0.0f)
         {
             if(chosenSituation == "Run")
             {
                 Debug.Log("CORRECT");
                 Pressed = true;
+                BattleText.text = "You ran away.";
             }
             else
             {
                 Debug.Log("WRONG");
                 Pressed = true;
+                BattleText.text = "The Goblin Attacks!";
+                StartCoroutine(RunWrong());
             }
         }
     }
@@ -103,5 +130,71 @@ public class RPGHandler : MonoBehaviour
     {
         HPText.text = "HP: 1";
         HPText.color = Color.red;
+    }
+
+
+
+    IEnumerator FightCorrect()
+	{
+        yield return new WaitForSeconds(2f);
+        BattleText.text = "The Goblin was defeated.";
+        S_Goblin_256x256.SetActive(false);
+        GoblinText.gameObject.SetActive(false);
+        yield return new WaitForSeconds(2f);
+        BattleText.text = "You Won!!!";
+    }
+
+    IEnumerator FightWrong()
+	{
+        yield return new WaitForSeconds(2f);
+        HPText.text = "HP: 0";
+        HPText.color = Color.red;
+        BattleText.text = "You lost all your HP.";
+        yield return new WaitForSeconds(2f);
+        BattleText.text = "Game Over.";
+    }
+
+    IEnumerator HealCorrect()
+	{
+        yield return new WaitForSeconds(2f);
+        BattleText.text = "The Goblin died due to poison.";
+        S_Goblin_256x256.SetActive(false);
+        GoblinText.gameObject.SetActive(false);
+        yield return new WaitForSeconds(2f);
+        BattleText.text = "You Won!!!";
+    }
+
+    IEnumerator HealWrong()
+	{
+        yield return new WaitForSeconds(2f);
+        HPText.text = "HP: 0";
+        HPText.color = Color.red;
+        BattleText.text = "You lost all your HP.";
+        yield return new WaitForSeconds(2f);
+        BattleText.text = "Game Over.";
+    }
+
+    IEnumerator RunWrong()
+	{
+        yield return new WaitForSeconds(2f);
+        HPText.text = "HP: 0";
+        HPText.color = Color.red;
+        BattleText.text = "You lost all your HP.";
+        yield return new WaitForSeconds(2f);
+        BattleText.text = "Game Over.";
+    }
+
+    IEnumerator TimeUp()
+	{
+        yield return new WaitForSeconds(0.1f);
+        BattleText.text = "You took too long.";
+        yield return new WaitForSeconds(2f);
+        BattleText.text = "The Goblin Attacks!";
+        yield return new WaitForSeconds(2f);
+        HPText.text = "HP: 0";
+        HPText.color = Color.red;
+        BattleText.text = "You lost all your HP.";
+        yield return new WaitForSeconds(2f);
+        BattleText.text = "Game Over.";
     }
 }
